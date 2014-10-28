aef4241cc3b7515428e3c7d0841151450201a390
Balaji <rajanbalaji2000@yahoo.co.in>
2014-10-28 17:07:23 +0530

    First step to get rid hard coded evaluate() method

--- Test-Largest/src/code/Template.java
+++ Test-Largest/src/code/Template.java
@@ -3,6 +3,8 @@ package code;
 public class Template
 {
 
+	private String variableValue; 
+	
 	public Template(String templateText)
 	{
 		// TODO Auto-generated constructor stub
@@ -10,14 +12,14 @@ public class Template
 
 	public void set(String variable, String value)
 	{
-		// TODO Auto-generated method stub
+		this.variableValue = value;
 		
 	}
 
 	public String evaluate()
 	{
 		// TODO Auto-generated method stub
-		return "Hello, Balaji";
+		return "Hello, " + this.variableValue;
 	}
 
 }
